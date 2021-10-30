aad3efc68cb6ffd77dd08ce89d370bb4a7cbb9ab
Your Name <you@example.com>
2021-10-30 15:20:45 +0300

    Force autopip8 formating. Speech to text with downloading files into specifing directory with removing them after job is done
    [changelog]:
    added: autopip8 formating
    added: Models downloading at start
    added: downloading audio in specific directory
    updated: gitignore

---
+++ pyfilter_audio/__init__.py
@@ -0,0 +1,2 @@
+from . import speech_to_text_core
+from . import speech_to_text_vosk
--- pyfilter_audio/__main__.py
+++ pyfilter_audio/__main__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # coding --utf-8--
 
+from loguru import logger
 from speech_to_text_vosk import VoskSpeechToText
 
 if __name__ == "__main__":
     vosk_model = VoskSpeechToText()
-    vosk_model.get_text_from_audio(
-        "file:///home/ristle/Programming/speech-to-text/fr/audio/my_result.wav")
+    logger.info(vosk_model.get_text_from_audio(
+                "file:///home/ristle/Programming/speech-to-text/fr/audio/my_result.wav"))
